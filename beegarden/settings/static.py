eef91df421b533e42c73a82e2a3ac6d7e9492c05
Sirkirill <sirbarkalov@gmail.com>
2020-10-27 09:55:35 +0200

    fix

--- beegarden/settings/static.py
+++ beegarden/settings/static.py
@@ -2,8 +2,5 @@ import os
 
 from settings import BASE_DIR
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

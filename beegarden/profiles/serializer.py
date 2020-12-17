4a08f537532c9a610c0bafac41d96951bf78ae55
Sirkirill <sirbarkalov@gmail.com>
2020-12-18 00:31:04 +0200

    fix

--- beegarden/profiles/serializer.py
+++ beegarden/profiles/serializer.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 
 from profiles.models import Product, User
+from django_restql.mixins import DynamicFieldsMixin
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', "email", "phone")
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
 
     class Meta:
-        creator = UserSerializer(read_only=True)
+        creator = UserSerializer(read_only=True, fields=["email"])
 
         model = Product
         fields = ("id", "name", "description", "creator", "price", "location")

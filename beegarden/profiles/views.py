184e5a93c74c2e5bc67e6ee46a7a858835d466ce
Sirkirill <sirbarkalov@gmail.com>
2020-12-22 20:07:11 +0200

    fix

--- beegarden/profiles/views.py
+++ beegarden/profiles/views.py
@@ -185,7 +185,7 @@ def confirm_email(request, key):
 def is_authenticated(request):
     if request.user.is_authenticated:
         dataset = User.objects.all()
-        serializer = UserAunSerializer(instance=dataset)
+        serializer = UserSerializer(instance=dataset)
         return Response(serializer.data)
     else:
         return Response(status=401)
